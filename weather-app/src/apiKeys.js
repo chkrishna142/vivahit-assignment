2d9662eba7d79e92fb0ebe1ae6c399af917f6a75
chkrishna142 <chkrishna142@gmail.com>
2023-07-07 13:10:52 +0530

    all done

---
+++ weather-app/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "aae99e98e9195fef8c9542f4a18a0a19",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
